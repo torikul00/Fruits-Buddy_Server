310af3193ccf29792ff365101bdd4f67884f1e66
Torikul <toriqulislam142@gmail.com>
2022-05-01 09:29:57 -0500

    create api for all items

--- index.js
+++ index.js
@@ -2,20 +2,42 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000
 const app = express()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
 
 
 
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.jauhp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect()
+        const collections = client.db('fruitsBuddy').collection('fruits');
+        app.get('/fruits', async (req, res) => {
+            const query = {}
+            const cursor = collections.find(query)
+            const fruits = await cursor.toArray()
+            res.send(fruits)
+     
+        })
+       
+    }
+   
+    finally {
+        
+    }
+}
+run()
 
-
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
     res.send('Server is running')
 })
 
 
-app.listen(port ,()=>{
+app.listen(port, () => {
     console.log('Listening port ', port)
 })
 
