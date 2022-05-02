3f6ebdc5ae5435eaefcf087c4efe7df39e59096b
Torikul <toriqulislam142@gmail.com>
2022-05-02 02:51:47 -0500

    created delete api

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ async function run() {
     try {
         await client.connect()
         const collections = client.db('fruitsBuddy').collection('fruits');
+        // get all items api
         app.get('/fruits', async (req, res) => {
             const query = {}
             const cursor = collections.find(query)
@@ -23,6 +24,7 @@ async function run() {
             res.send(fruits)
      
         })
+        // Single item info api
         app.get('/fruits/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
@@ -30,9 +32,17 @@ async function run() {
             res.send(fruit)
         })
 
+        // create new item api
         app.post('/fruits', async (req, res) => {
             const data = req.body
             const result =await collections.insertOne(data)
+            res.send(result)
+        })
+        // delete a item 
+        app.delete('/fruit/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {_id:ObjectId(id)}
+            const result = await collections.deleteOne(query)
             res.send(result)
         })
     }
@@ -43,7 +53,7 @@ async function run() {
 }
 run()
 
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Server is running')
 })
 
